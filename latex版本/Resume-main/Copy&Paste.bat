93145f927bb2b2d9e8dbdbb409991929f2d829ec
liuyao <1490440300@qq.com>
2021-07-18 13:22:12 +0800

    add latex resume.

---
+++ latex版本/Resume-main/Copy&Paste.bat
@@ -0,0 +1,9 @@
+chcp 65001
+
+@echo off 
+
+copy "resume-zh.pdf" "C:\Users\Kai\OneDrive\Interviews\0-个人简历.pdf"
+
+copy "resume-zh.pdf" "C:\Users\Kai\OneDrive - CodingCatDev\Public\Resume\Resume.pdf"
+
+exit
